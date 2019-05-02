721676250349b626a4aa785b16acbcbd9943ba22
MaximeRoux <maxroux@gmail.com>
2019-05-02 10:37:45 +0200

    Update and rename bench_vexcl.cpp to bench_expression_template.cpp

--- bench_expression_template.cpp
+++ bench_expression_template.cpp
@@ -1,11 +1,17 @@
-void bench() {
+#include <stdio.h>
+#include <stdexcept>
+#include <iostream>
+#define VEXCL_BACKEND_CUDA
 
-	std::vector<float> a(n), b(n), c(n), d(n); // Host vector.
-  
+#include <vexcl/vexcl.hpp>
+#include <vexcl/vector_view.hpp>
+
+void bench() {
+	std::vector<float> a(n), b(n), c(n), d(n); // Host vector
 	vex::Context ctx(vex::Filter::GPU); //Contexte
 	vex::vector<float> a_d(n), b_d(n), c_d(n), res_d(n);  // Device vector.
 	
-  for (size_t i = 0; i < n; i++)
+  	for (size_t i = 0; i < n; i++)
 	{
 		a[i] = (i*1.0f + 4.)*0.5;
 		b[i] = (i*1.0f + 5.)*0.5;
