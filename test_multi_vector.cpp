ffc4447e6fcc9462b5b1e862092adfb6d4c54803
MaximeRoux <maxroux@gmail.com>
2019-05-29 10:39:46 +0200

    Update test_multi_vector.cpp

--- test_multi_vector.cpp
+++ test_multi_vector.cpp
@@ -5,8 +5,10 @@ void test_multi_vector(){
 	using type = vex::multivector<double, 3>;
   
 	type data[5];
-	data[2](0).resize(size);
+	data[2].resize(size);
 	data[2](0) = 10;
+	data[2](1) = 10;
+	data[2](2) = 10;
   
 	vex::copy(data[2](0), a); //copy device to host
 }
